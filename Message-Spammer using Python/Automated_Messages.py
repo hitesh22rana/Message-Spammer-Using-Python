0898bc1b681292d174c05b9dbe439122d5061e68
Hitesh Rana <85160299+hitesh22rana@users.noreply.github.com>
2021-08-19 14:59:44 +0530

    Update Automated_Messages.py

--- Message-Spammer using Python/Automated_Messages.py
+++ Message-Spammer using Python/Automated_Messages.py
@@ -1,17 +1,17 @@
-# import time
-# import pyautogui
+import time
+import pyautogui
 
-# def Sendmessages():
-#     time.sleep(4)
-#     text = open('messages.txt')
-#     for each_line in text:
-#         pyautogui.typewrite(each_line)
-#         pyautogui.press("enter")
+def Sendmessages():
+    time.sleep(4)
+    text = open('messages.txt')
+    for each_line in text:
+        pyautogui.typewrite(each_line)
+        pyautogui.press("enter")
 
 
-# if __name__ == '__main__':
-#     Sendmessages()
+if __name__ == '__main__':
+    Sendmessages()
 
-# with open(r"messages.txt","w") as f:
-#     for i in range(100):
-#         f.write("Hi\n")
+with open(r"messages.txt","w") as f:
+    for i in range(100):
+        f.write("Hi\n")
