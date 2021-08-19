6e8468cb3a889900c36e55fd0fb22a3eb4f35b76
Hitesh <hitesh22rana@gmail.com>
2021-08-19 15:11:46 +0530

    added more flexibility in the code

--- Message-Spammer using Python/Automated_Messages.py
+++ Message-Spammer using Python/Automated_Messages.py
@@ -8,10 +8,15 @@ def Sendmessages():
         pyautogui.typewrite(each_line)
         pyautogui.press("enter")
 
+def generate_message(text,total):
+    
+    with open(r"messages.txt","w") as f:
+        for i in range(total):
+            f.write(text+"\n")
 
 if __name__ == '__main__':
+    text = str(input("Enter the text you want to Spam : "))
+    total = int(input("Enter how many time you want to spam : "))
+    generate_message(text,total)
     Sendmessages()
 
-with open(r"messages.txt","w") as f:
-    for i in range(100):
-        f.write("Hi\n")
